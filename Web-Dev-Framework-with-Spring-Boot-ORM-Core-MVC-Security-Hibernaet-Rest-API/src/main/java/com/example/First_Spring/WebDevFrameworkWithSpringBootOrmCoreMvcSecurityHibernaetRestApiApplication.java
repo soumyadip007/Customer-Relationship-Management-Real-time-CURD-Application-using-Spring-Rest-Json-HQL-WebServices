9072142b9405190fb63a7a73611987d49d6db93d
soumyadip007 <soumyadip.note@gmail.com>
2019-05-17 19:39:21 +0530

    Setup Environment

---
+++ Web-Dev-Framework-with-Spring-Boot-ORM-Core-MVC-Security-Hibernaet-Rest-API/src/main/java/com/example/First_Spring/WebDevFrameworkWithSpringBootOrmCoreMvcSecurityHibernaetRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.First_Spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebDevFrameworkWithSpringBootOrmCoreMvcSecurityHibernaetRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebDevFrameworkWithSpringBootOrmCoreMvcSecurityHibernaetRestApiApplication.class, args);
+	}
+
+}
