7d9dba8ce92bfa2a94c91412f7b22e16c45fa70b
soumyadip007 <soumyadip.note@gmail.com>
2019-05-18 21:32:07 +0530

    Auto Generated Class //INterface

---
+++ Spring_Core/src/main/java/com/example/Spring_learn/TrackCoach.java
@@ -0,0 +1,11 @@
+package com.example.Spring_learn;
+
+public class TrackCoach implements Coach {
+
+	@Override
+	public String getDailyWorkout() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
