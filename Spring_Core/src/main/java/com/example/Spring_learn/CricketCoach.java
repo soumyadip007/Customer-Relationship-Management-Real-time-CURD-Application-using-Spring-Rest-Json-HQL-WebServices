a67e1c0164b4646dfc676d28af9ea886c2b08a49
soumyadip007 <soumyadip.note@gmail.com>
2019-05-18 21:14:14 +0530

    1st Commit //Class

---
+++ Spring_Core/src/main/java/com/example/Spring_learn/CricketCoach.java
@@ -0,0 +1,9 @@
+package com.example.Spring_learn;
+
+public class CricketCoach {
+
+	public String getDailyWorkout()
+	{
+		return "PLay 30 mins daily";
+	}
+}
---
+++ Spring_Core/src/main/java/com/example/Spring_learn/MyApp.java
@@ -0,0 +1,11 @@
+package com.example.Spring_learn;
+
+public class MyApp {
+
+	public static void main(String args[])
+	{
+		//Create OBJ & Use OBJ
+		CricketCoach obj=new CricketCoach();
+		System.out.print(obj.getDailyWorkout());
+	}
+}
