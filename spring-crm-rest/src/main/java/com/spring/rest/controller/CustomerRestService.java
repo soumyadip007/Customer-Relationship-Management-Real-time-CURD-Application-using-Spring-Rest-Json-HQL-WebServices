703a12bccae606d9bfcbec7ac3533fca4946284d
soumyadip007 <soumyadip.note@gmail.com>
2019-05-30 12:53:58 +0530

    Mapping added

--- spring-crm-rest/src/main/java/com/spring/rest/controller/CustomerRestService.java
+++ spring-crm-rest/src/main/java/com/spring/rest/controller/CustomerRestService.java
@@ -1,9 +1,13 @@
 package com.spring.rest.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.spring.rest.entity.Customer;
 import com.spring.rest.service.CustomerService;
 
 @RestController
@@ -15,4 +19,12 @@ public class CustomerRestService {
 	private CustomerService customerService;
 	
 
+	@GetMapping("/customers")
+	public List<Customer> getCustomers()
+	{
+		
+		return customerService.getCustomers();
+	}
+	
+	
 }
