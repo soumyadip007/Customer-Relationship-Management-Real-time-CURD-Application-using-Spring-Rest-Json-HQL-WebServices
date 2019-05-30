3484bd14c05b7e520e9bc26b619a3952a15ecf8e
soumyadip007 <soumyadip.note@gmail.com>
2019-05-30 13:14:03 +0530

    Get Single Customer

--- spring-crm-rest/src/main/java/com/spring/rest/controller/CustomerRestService.java
+++ spring-crm-rest/src/main/java/com/spring/rest/controller/CustomerRestService.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,6 +25,15 @@ public class CustomerRestService {
 	{
 		
 		return customerService.getCustomers();
+	}
+	
+	@GetMapping("/customerId")
+	public Customer customerId(@PathVariable int id)
+	{
+		
+		Customer obj=customerService.getCustomer(id);
+		
+		return obj;
 	}
 	
 	
