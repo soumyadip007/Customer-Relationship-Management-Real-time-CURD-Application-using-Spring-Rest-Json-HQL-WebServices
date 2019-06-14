18fee7eebbab45b0301067ea9d1a68ab8e3d09a3
soumyadip007 <soumyadip.note@gmail.com>
2019-06-14 14:10:17 +0530

    Connected to angular (Yaaah-HOO)

--- spring-crm-rest/src/main/java/com/spring/rest/controller/CustomerRestService.java
+++ spring-crm-rest/src/main/java/com/spring/rest/controller/CustomerRestService.java
@@ -3,6 +3,7 @@ package com.spring.rest.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,6 +17,7 @@ import com.spring.rest.entity.Customer;
 import com.spring.rest.exception.CustomerNotFoundException;
 import com.spring.rest.service.CustomerService;
 
+@CrossOrigin(origins ="http://localhost:4200") 
 @RestController
 @RequestMapping("/api")
 public class CustomerRestService {
